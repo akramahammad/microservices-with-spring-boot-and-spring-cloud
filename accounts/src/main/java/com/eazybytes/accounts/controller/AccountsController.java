c488d033286fb4581828b0b0a6cc5e394d8c6393
Akram Ahamed <akram.ahammad@gmail.com>
2022-07-23 16:56:39 +0530

    Added accounts, loans and cards microservices

---
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.eazybytes.accounts.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.accounts.model.Accounts;
+import com.eazybytes.accounts.model.Customer;
+import com.eazybytes.accounts.repository.AccountsRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class AccountsController {
+	
+	@Autowired
+	private AccountsRepository accountsRepository;
+	
+	@Value("${accounts.msg}")
+	private String msg;
+
+	@PostMapping("/myAccount")
+	public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		if (accounts != null) {
+			return accounts;
+		} else {
+			return null;
+		}
+
+	}
+	
+	@GetMapping("/msg")
+	public String getMessage() {
+		return this.msg;
+	}
+	
+
+}
