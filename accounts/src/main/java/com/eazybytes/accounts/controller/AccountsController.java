3cfecd81ade6c0eb6aa602bb2b4551889297c87d
Akram Ahamed <akram.ahammad@gmail.com>
2022-07-30 22:53:40 +0530

    Added micrometer, prometheus and grafana changes to monitor services

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -25,6 +25,7 @@ import com.eazybytes.accounts.repository.AccountsRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.micrometer.core.annotation.Timed;
 
 /**
  * @author Eazy Bytes
@@ -46,6 +47,7 @@ public class AccountsController {
 	private String msg;
 
 	@PostMapping("/myAccount")
+	@Timed(value = "getAccountDetails.time", description = "Time taken to get Account Details")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
 
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
