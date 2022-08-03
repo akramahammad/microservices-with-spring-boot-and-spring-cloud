56c84b437aa0d0fa1f911a3d7dc37e05796a88e6
Akram Ahamed <akram.ahammad@gmail.com>
2022-08-04 02:06:30 +0530

    Securing microservices using keycloak

--- gateway/src/main/java/com/microservices/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/microservices/gateway/GatewayApplication.java
@@ -1,8 +1,13 @@
 package com.microservices.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +15,32 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@Autowired
+	private TokenRelayGatewayFilterFactory filterFactory;
+	
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes()
+	        .route(p -> p
+	            .path("/bank/accounts/**")
+	            .filters(f -> f.filters(filterFactory.apply())
+					.rewritePath("/bank/accounts/(?<segment>.*)","/${segment}")
+					.removeRequestHeader("Cookie"))
+	            .uri("lb://ACCOUNTS")).
+	        route(p -> p
+		            .path("/bank/loans/**")
+					.filters(f -> f.filters(filterFactory.apply())
+							.rewritePath("/bank/loans/(?<segment>.*)","/${segment}")
+							.removeRequestHeader("Cookie"))
+		            .uri("lb://LOANS")).
+	        route(p -> p
+		            .path("/bank/cards/**")
+					.filters(f -> f.filters(filterFactory.apply())
+							.rewritePath("/bank/cards/(?<segment>.*)","/${segment}")
+							.removeRequestHeader("Cookie"))
+		            .uri("lb://CARDS")).build();
 	}
 
 }
